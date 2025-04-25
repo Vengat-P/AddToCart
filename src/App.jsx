da0e2ce164944addd8e8a5ecdf359bf721fc7109
Vengat-P <cronyvengat33@gmail.com>
2025-04-25 07:55:56 +0530

    Base setup completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Heading from './Components/Heading/Navbar'
+import Content from './Components/Content/Content'
+const App = () => {
+  return (
+    <div>
+      <Content/>
+      <Heading/>
+    </div>
+  );
+};
+
+export default App;
